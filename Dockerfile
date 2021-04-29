57b6d69a40e6fe2188433454d3d6b992341922b2
dkulagin <dkulagin.com@gmail.com>
2021-04-29 23:16:07 +0500

    feat(pf-13): Add swipe pages
    - fix nginx.conf
    - fix Dockerfile
    - fix header height on mobile

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,5 @@ RUN npm run build
 # serve app with nginx server
 FROM nginx:latest
 COPY --from=build /app/dist/pf /usr/share/nginx/html
-COPY --from=build /app/scripts/nginx/nginx.conf /etc/nginx/
-COPY --from=build /app/scripts/nginx/nginx.conf /etc/nginx/conf.d/default.conf
 EXPOSE 80
+EXPOSE 443
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
+import { NavigationEnd, Router } from '@angular/router';
+import { mergeMap, debounceTime, take, filter, takeUntil } from 'rxjs/operators';
 import { AuthService } from '@services/auth.service';
 import { navlist } from './models/navbar';
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, OnInit, OnDestroy } from '@angular/core';
+import { fromEvent,ReplaySubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +11,71 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
   navlist = navlist;
 
+  private url: string = '/games';
+
+  $destroyer: ReplaySubject<void> = new ReplaySubject<void>(1);
+
   constructor(
-    public authService: AuthService
+    public authService: AuthService,
+    private router: Router
   ) { }
 
+  ngOnDestroy(): void {
+    this.$destroyer.next();
+  }
+
+  ngAfterViewInit(): void {
+    this.listenTouches();
+  }
+
   ngOnInit(): void {
+    this.url = this.router.url;
+    
+    this.router.events.pipe(
+      takeUntil(this.$destroyer),
+      filter(event => {
+        return event instanceof NavigationEnd;
+      })
+    ).subscribe((event: NavigationEnd) => {
+      this.url = event.url;
+    });
   }  
+
+  private listenTouches(): void {
+    let startX = 0;
+    let endX = 0;
+    fromEvent(document, 'touchstart').pipe(
+      takeUntil(this.$destroyer),
+      mergeMap((touchEvent: TouchEvent) => {
+        startX = touchEvent.touches[0].clientX;
+        return fromEvent(document, 'touchmove');
+      }),
+      debounceTime(50),
+      take(1)
+    ).subscribe((touchEvent: TouchEvent) => {
+      endX = touchEvent.touches[0].clientX;
+      if ((endX - startX) > Math.abs(50)) {
+        this.swipePage(-1);        
+      } else if ((startX - endX) > Math.abs(50)) {
+        this.swipePage(1);
+      }
+
+      this.listenTouches();
+    });
+  }
+
+  private swipePage(shift: number): void {
+    let index = this.navlist.findIndex(value => value.url === this.url);
+
+    index = index + shift;
+
+    if (index < 0 || index >= this.navlist.length) {
+      index = index - shift;
+    }
+
+    this.router.navigateByUrl(this.navlist[index]?.url || '/games');
+  }
 }
